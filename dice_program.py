55807fa9df617fa2f91b126a988ce828dd1a631f
Andy Quinn <andy.kettler.quinn@gmail.com>
2023-03-30 19:45:58 -0500

    initial commit

---
+++ dice_program.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+This is an attempt at a dice rolling application thingy
+that may someday evolve into something potentially useful...
+"""
+
+__author__ = "Andy Quinn"
+__version__ = "0.1.0"
+__license__ = "MIT"
+
+
+def main():
+    print("Starting app")
+
+    # while True:
+
+    print("Ending app")
+
+
+def roll_dice(num_of_sides: int):
+    print(f"Rolling a {num_of_sides}-sided dice")
+
+
+if __name__ == "__main__":
+    main()
